b5db77f0e169c20fc6cc6794c18ea567362c0bcc
Bernadette Davis <bsd231@nyu.edu>
2019-12-23 22:32:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Add your routes here
+<<<<<<< HEAD
   
   root to: 'welcome#home'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+=======
+  root to: 'welcome#home'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+>>>>>>> f9859d8c3b5f73a9ced6955580b2b73617bd1b89
 end
